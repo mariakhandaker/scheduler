b87e36d173d3d971951e3d1fb802b453adff022e
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-03 11:50:46 -0400

    appointment empty/show stories added

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header";
-import Show from "components/Appointment/Show";
-import Empty from "components/Appointment/Empty";
+import "./styles.scss";
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article> 
+  const { time, interview } = props;
+  return (
+    <article className="appointment"> 
+      <Header time={time}/>
+      {interview ? <Show interviewer={interview.interviewer} student={interview.student} /> : <Empty />} 
+    </article>
+  )
 }
-
