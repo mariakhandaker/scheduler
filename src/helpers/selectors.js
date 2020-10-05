158c70930e39a5d5ee6680c8ce14bc06ea1b1e95
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-05 17:22:34 -0400

    first selector function :)

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,1 +1,11 @@
-function getAppointmentsForDay()
+export function getAppointmentsForDay(state, day) {
+  const appointments = [];
+  state.days.forEach((weekday) => {
+    if (weekday.name === day) {
+      weekday.appointments.forEach((appointment) => {
+        appointments.push(state.appointments[appointment])
+      })
+    }
+  })
+  return appointments.length ? appointments : []
+};
