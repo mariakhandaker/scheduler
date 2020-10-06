2a83a10a11a60a5083fa4aaf86a285f19c615750
Maria Khandaker <mariakhandaker@gmail.com>
2020-10-06 16:14:20 -0400

    first two useVisualMode functions working!!

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,18 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode } 
-}
+  const [history, setHistory] = useState([initial]);
+  
+  const transition = (newMode, replace = false) => {
+    setMode(newMode);
+    replace ? setHistory(prev => [newMode, ...prev.slice(1)]) : setHistory(prev => [newMode, ...prev])
+  }
+  
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[1]);
+      setHistory(prev => prev.slice(1))
+    }            
+  }
+  return { mode, transition, back }; 
+};
